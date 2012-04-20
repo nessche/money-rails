30458ca5658417ca89dec46ecb1746dd5a2b2140
Kristian Mandrup <kmandrup@gmail.com>
2012-04-20 20:21:11 +0200

    avoid auto orm add

--- lib/money-rails/railtie.rb
+++ lib/money-rails/railtie.rb
@@ -1,9 +1,13 @@
 module MoneyRails
   module Monetizable
     class Railtie < ::Rails::Railtie
-			[:active_record, :mongoid, :mongo_mapper, :data_mapper, :sequel].each do |orm|
+			[:active_record].each do |orm|
 		  	MoneyRails::Orm.send(orm)
-		  end		  
+		  end
+
+		  def self.orms
+		  	[:active_record, :mongoid, :mongo_mapper, :data_mapper, :sequel]
+		  end
     end
   end
 end
